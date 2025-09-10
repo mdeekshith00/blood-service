ef0512b05811160239d6b851e545d9fa668fa373
mdeekshith00 <mdeekshith000@gmail.com>
2025-09-11 00:25:51 +0530

    added circuit breaker:

--- src/main/java/com/blood_service/BloodServiceApplication.java
+++ src/main/java/com/blood_service/BloodServiceApplication.java
@@ -2,8 +2,10 @@ package com.blood_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BloodServiceApplication {
 
 	public static void main(String[] args) {
